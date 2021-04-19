d184d9368a7235ca44392c4111341c7e7d5b0524
Maggie Hibberd <maggiehibberd@gmail.com>
2021-04-19 15:39:53 +0100

    dependency injection with Game class. Tests passsing

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -15,20 +17,28 @@ get '/' do
 end
 
 post '/names' do 
-  $player_1 = Player.new(params[:player_1_name])
-  $player_2 = Player.new(params[:player_2_name]) 
+
+  # $player_1 = Player.new(params[:player_1_name])
+  # $player_2 = Player.new(params[:player_2_name]) 
+  player_1 = Player.new(params[:player_1_name])
+  player_2 = Player.new(params[:player_2_name])
+  $game = Game.new(player_1, player_2)
   redirect '/play'
 end
 
 get '/play' do 
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
+  # @player_1_name = $player_1
+  # @player_2_name = $player_2
+  @game = $game
   erb :play 
 end
 
 get '/attack' do
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
+  # @player_1_name = $player_1
+  # @player_2_name = $player_2
+  # Game.new.game_attack(@player_2_name)
+  @game = $game
+  @game.attack(@game.player_2)
   erb :attack
 end
 
